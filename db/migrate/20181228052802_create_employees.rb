24d95829ca697fd23937d9df1bacc67f7b04d79c
CrisnaPreethi <crisnapreethi1992@gmail.com>
2018-12-28 11:45:41 +0530

    We'll see

---
+++ db/migrate/20181228052802_create_employees.rb
@@ -0,0 +1,13 @@
+class CreateEmployees < ActiveRecord::Migration[5.1]
+  def change
+    create_table :employees do |t|
+      t.string :name
+      t.integer :age
+      t.string :gender
+      t.string :designation
+      t.string :email
+
+      t.timestamps
+    end
+  end
+end
